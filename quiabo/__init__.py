0b12f56480e16017ef1f2279151f638b325206ac
María A. Matienzo <matienzo@berkeley.edu>
2026-09-11 12:08:57 -0700

    get flask app config from prefixed env variables

--- quiabo/__init__.py
+++ quiabo/__init__.py
@@ -5,19 +5,9 @@ from flask import Flask
 from quiabo import health, root
 from quiabo.celery import celery_init_app
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-REDIS_URL = os.getenv("REDIS_URL")
-
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.config.from_mapping(
-        CELERY=dict(
-            broker_url=REDIS_URL,
-            result_backend=DATABASE_URL,
-            task_default_queue="quiabo",
-            task_ignore_result=False,
-        ),
-    )
+    app.config.from_prefixed_env(prefix="QUIABO")
 
     app.register_blueprint(root.bp)
     app.register_blueprint(health.bp)
